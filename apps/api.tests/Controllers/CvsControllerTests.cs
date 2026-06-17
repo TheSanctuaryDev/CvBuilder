43b41fed969205821e659a9b87e3bce4f39b421f
TheSanctuaryDev <thesanctuarydev29@gmail.com>
2026-06-17 09:50:21 +0100

    test: ajouter tests GET{id} 404 et DELETE 204 pour CvsController

--- apps/api.tests/Controllers/CvsControllerTests.cs
+++ apps/api.tests/Controllers/CvsControllerTests.cs
@@ -53,4 +53,51 @@ public class CvsControllerTests(TestWebApplicationFactory factory)
         cv!.Title.Should().Be("Mon CV Dev");
         cv.TemplateKey.Should().Be("free/_mod-1");
     }
+
+    [Fact]
+    public async Task GetCv_OtherUsersCv_Returns404()
+    {
+        // Arrange: User A creates a CV
+        var userA = Guid.NewGuid();
+        _client.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Bearer", TestJwtHelper.GenerateToken(userA));
+
+        var request = new CreateCvRequest("User A CV", "free/_mod-1", false);
+        var createResponse = await _client.PostAsJsonAsync("/api/cvs", request);
+        var cvDto = await createResponse.Content.ReadFromJsonAsync<CvDto>();
+        var cvId = cvDto!.Id;
+
+        // Act: User B tries to access User A's CV
+        var userB = Guid.NewGuid();
+        _client.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Bearer", TestJwtHelper.GenerateToken(userB));
+
+        var response = await _client.GetAsync($"/api/cvs/{cvId}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task DeleteCv_ValidAuth_Returns204()
+    {
+        // Arrange: Create a CV
+        var userId = Guid.NewGuid();
+        _client.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Bearer", TestJwtHelper.GenerateToken(userId));
+
+        var request = new CreateCvRequest("CV to Delete", "free/_mod-1", false);
+        var createResponse = await _client.PostAsJsonAsync("/api/cvs", request);
+        var cvDto = await createResponse.Content.ReadFromJsonAsync<CvDto>();
+        var cvId = cvDto!.Id;
+
+        // Act: Delete the CV
+        var response = await _client.DeleteAsync($"/api/cvs/{cvId}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
 }
